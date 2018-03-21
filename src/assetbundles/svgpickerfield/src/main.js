1ede9bb1b96598df54a1af74f0bdcb83c4747c2b
Jinzhe <jinzhe@simple.com.au>
2018-03-22 09:28:31 +1030

    Update style
    Make svg icons bigger in the field dropdown

--- src/assetbundles/svgpickerfield/src/main.js
+++ src/assetbundles/svgpickerfield/src/main.js
@@ -7,10 +7,10 @@ const initSvgPicker = (context) => {
         options: svgs,
         render:{
             option: function(data,escape) {
-                return `<div style="display:flex;align-items:center;"><svg style="height:24px" viewBox="${data.viewBox}">${data.symbol}</svg><span style="padding-left:10px;">${data.id}</span></div>`;
+                return `<div style="display:flex;align-items:center;"><svg style="height:36px" viewBox="${data.viewBox}">${data.symbol}</svg><span style="padding-left:10px;">${data.id}</span></div>`;
             },
             item: function(data,escape) {
-                return `<div style="display:flex;align-items:center;"><svg style="height:24px" viewBox="${data.viewBox}">${data.symbol}</svg><span style="padding-left:10px;">${data.id}</span></div>`;
+                return `<div style="display:flex;align-items:center;"><svg style="height:36px" viewBox="${data.viewBox}">${data.symbol}</svg><span style="padding-left:10px;">${data.id}</span></div>`;
             }
         }
     })[0].selectize;
