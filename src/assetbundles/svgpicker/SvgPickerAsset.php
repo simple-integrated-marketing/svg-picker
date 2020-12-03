7f12845988589eeb97781b738f904723a8abd7d9
Jinzhe <jinzhe@simple.com.au>
2020-12-03 14:37:46 +1030

    psr-4

--- src/assetbundles/svgpicker/SvgPickerAsset.php
+++ src/assetbundles/svgpicker/SvgPickerAsset.php
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) 2018 Simple Integrated Marketing
  */
 
-namespace simpleteam\svgpicker\assetbundles\svgpick;
+namespace simpleteam\svgpicker\assetbundles\svgpicker;
 
 use Craft;
 use craft\web\AssetBundle;
